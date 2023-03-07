0e450d8d1401eabf658b15b9f41385cddb863b9e
loudringphone <loudringphone@gmail.com>
2023-03-08 10:40:34 +1100

    times

---
+++ workday-management/src/components/CreateTime.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, toRefs, defineProps, onMounted, watch } from "vue";
+import { supabase } from "../lib/supabaseClient";
+
+const props = defineProps({
+  createForm: Boolean,
+});
+const { createForm } = toRefs(props);
+console.log(` child component - createForm`, createForm.value);
+const newTime = {
+  start_date: "",
+  time: "",
+  course: "",
+  tutor_id: "",
+};
+
+watch(createForm, (newValue, oldValue) => {
+  getAllTimes();
+});
+</script>
+<template>
+    <div v-if="!createForm"><p>no</p></div>
+    <div v-if="createForm"><p>yes</p></div>
+</template>
