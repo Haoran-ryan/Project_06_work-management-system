85f161ceda9af7a867ed61f83eecbbb57e248bea
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-03-08 15:23:53 +1100

    course edit fully functioning

--- workday-management/src/components/CourseIndex.vue
+++ workday-management/src/components/CourseIndex.vue
@@ -1,33 +1,75 @@
 <template>
-  <div class="q-pa-xl" v-if="allCourses">
-    <h3 class="text-h5">All Courses</h3>
-    <q-list>
-      <q-item v-for="course in allCourses.value" :key="course.id">
-        <q-item-section>
-          <q-item-label>Course Name: {{ course.name }}</q-item-label>
-          <q-item-label caption>Duration: {{ course.duration }}</q-item-label>
-          <q-item-label caption
-            >Description: {{ course.description }}</q-item-label
-          >
-        </q-item-section>
-        <q-item-section side>
-          <q-btn
-            icon="edit"
-            :to="{ name: 'course_edit', params: { id: course.id } }"
-            s
+  <div class="container">
+    <button @click="displayCourses = !displayCourses">
+      {{ displayCourses ? "Create Course" : "All Courses" }}
+    </button>
+
+    <div class="q-pa-xl" v-if="allCourses && displayCourses">
+      <h3 class="text-h5">All Courses</h3>
+      <q-list>
+        <q-item v-for="course in allCourses.value" :key="course.id">
+          <q-item-section>
+            <q-item-label>Course Name: {{ course.name }}</q-item-label>
+            <q-item-label caption>Duration: {{ course.duration }}</q-item-label>
+            <q-item-label caption
+              >Description: {{ course.description }}</q-item-label
+            >
+          </q-item-section>
+          <q-item-section side>
+            <q-btn
+              icon="edit"
+              :to="{ name: 'course_edit', params: { id: course.id } }"
+              s
+            />
+            <q-btn icon="delete" @click="deleteCourse(course.id)" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+
+    <form v-else @submit.prevent="_submitForm">
+      <h3 class="text-h5">Create A Course</h3>
+      <div class="q-pa-md">
+        <div class="q-gutter-md" style="max-width: 300px">
+          <q-input
+            filled
+            label="Course Name"
+            type="text"
+            v-model="newCourse.name"
           />
-          <q-btn icon="delete" @click="deleteCourse(course.id)" />
-        </q-item-section>
-      </q-item>
-    </q-list>
+          <q-input
+            filled
+            label="Course Description"
+            type="text"
+            v-model="newCourse.description"
+          />
+          <q-input
+            filled
+            label="Course Duration"
+            type="number"
+            v-model="newCourse.duration"
+          />
+        </div>
+      </div>
+      <div class="q-pa-md q-gutter-sm">
+        <q-btn
+          style="background: #ff0080; color: white"
+          label="Create"
+          type="submit"
+          no-caps
+        />
+      </div>
+    </form>
   </div>
 </template>
 
 <script setup>
 import { supabase } from "src/lib/supabaseClient";
-import { reactive, onBeforeMount, onMounted } from "vue";
+import { reactive, onUpdated, onMounted, ref } from "vue";
 
 import CourseForm from "./CourseForm.vue";
+
+const displayCourses = ref(true);
 
 const allCourses = reactive({});
 
@@ -43,6 +85,34 @@ const getAllCourses = async () => {
 
 // getAllCourses();
 onMounted(getAllCourses);
+onUpdated(getAllCourses);
+
+// variable to store the new course data
+const newCourse = reactive({
+  name: "",
+  duration: null,
+  description: "",
+});
+
+function _submitForm() {
+  console.log(newCourse);
+  // insert newCourse into Supabase
+  const insertIntoSupabase = async function () {
+    let { data, error } = await supabase.from("courses").insert([
+      {
+        name: newCourse.name,
+        duration: newCourse.duration,
+        description: newCourse.description,
+      },
+    ]);
+  };
+  displayCourses.value = true;
+  insertIntoSupabase();
+  // clear the form
+  newCourse.name = "";
+  newCourse.duration = null;
+  newCourse.description = "";
+}
 </script>
 
 <script></script>
